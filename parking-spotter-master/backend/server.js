e222047693f5e179fb97148465cb9f9e2106dcf5
Aayush Mishra <122334900+aayushm02@users.noreply.github.com>
2025-09-28 02:05:43 +0530

    Update server.js

--- parking-spotter-master/backend/server.js
+++ parking-spotter-master/backend/server.js
@@ -76,13 +76,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-  });
-}
+
 
 const PORT = process.env.PORT || 5000;
 
